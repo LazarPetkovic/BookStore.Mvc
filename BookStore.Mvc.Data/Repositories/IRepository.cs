c51cc239a32a8882638bde5ddab4c16d92fb24a0
LazarPetkovic <42033361+LazarPetkovic@users.noreply.github.com>
2019-03-18 13:30:54 +0100

    Adding Repository Pattern

---
+++ BookStore.Mvc.Data/Repositories/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace BookStore.Mvc.Data.Repositories
+{
+    public interface IRepository<T> : IDisposable where T : class
+    {
+        IQueryable<T> All();
+
+        T GetById(object id);
+
+        void Add(T entity);
+
+        void Update(T entity);
+
+        void Delete(T entity);
+
+        void Delete(object id);
+
+        int SaveChanges();
+    }
+}
---
+++ BookStore.Mvc.Data/Repositories/Repository.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+
+namespace BookStore.Mvc.Data.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected IBookStoreDbContext Context { get; set; }
+        protected IDbSet<T> DbSet { get; set; }
+        public Repository(IBookStoreDbContext context)
+        {
+            this.Context = context ?? throw new ArgumentException("An instance of DbContext is required to use this repository.", "context");
+            this.DbSet = this.Context.Set<T>();
+        }
+
+        public virtual IQueryable<T> All()
+        {
+            return this.DbSet.AsQueryable();
+        }
+
+        public virtual T GetById(object id)
+        {
+            return this.DbSet.Find(id);
+        }
+
+        public virtual void Add(T entity)
+        {
+            var entry = this.Context.Entry(entity);
+            if (entry.State != EntityState.Detached)
+            {
+                entry.State = EntityState.Added;
+            }
+            else
+            {
+                this.DbSet.Add(entity);
+            }
+        }
+
+        public virtual void Update(T entity)
+        {
+            var entry = this.Context.Entry(entity);
+            if (entry.State == EntityState.Detached)
+            {
+                this.DbSet.Attach(entity);
+            }
+
+            entry.State = EntityState.Modified;
+        }
+
+        public virtual void Delete(T entity)
+        {
+            var entry = this.Context.Entry(entity);
+            if (entry.State != EntityState.Deleted)
+            {
+                entry.State = EntityState.Deleted;
+            }
+            else
+            {
+                this.DbSet.Attach(entity);
+                this.DbSet.Remove(entity);
+            }
+        }
+
+        public virtual void Delete(object id)
+        {
+            var entity = this.GetById(id);
+
+            if (entity != null)
+            {
+                this.Delete(entity);
+            }
+        }     
+
+        public int SaveChanges()
+        {
+            return this.Context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            this.Context.Dispose();
+        }
+    }
+}
