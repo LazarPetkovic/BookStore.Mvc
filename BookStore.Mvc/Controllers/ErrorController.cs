caa6520fbc3deb14c4719a1f33b85a40c7c9c805
LazarPetkovic <42033361+LazarPetkovic@users.noreply.github.com>
2019-03-18 21:36:34 +0100

    Error Controller

---
+++ BookStore.Mvc/Controllers/ErrorController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+
+namespace BookStore.Mvc.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult Error400()
+        {
+            return View();
+        }
+
+        public ActionResult Error404()
+        {
+            return View();
+        }
+
+        public ActionResult Error500()
+        {
+            return View();
+        }
+    }
+}
