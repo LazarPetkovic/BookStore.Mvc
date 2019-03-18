697b28a01138fa8252904a3fd9b82c7a9fb48bf4
LazarPetkovic <42033361+LazarPetkovic@users.noreply.github.com>
2019-03-18 21:32:34 +0100

    Category ViewModel

---
+++ BookStore.Mvc/ViewModels/Categories/CategoryViewModel.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using BookStore.Mvc.DataModel;
+using BookStore.Mvc.Mappings;
+
+namespace BookStore.Mvc.ViewModels.Categories
+{
+    public class CategoryViewModel : IMapFrom<Category>, IHaveCustomMappings
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public void CreateMappings(IMapperConfiguration configuration)
+        {
+            configuration.CreateMap<Category, CategoryViewModel>();
+
+
+        }
+    }
+}
