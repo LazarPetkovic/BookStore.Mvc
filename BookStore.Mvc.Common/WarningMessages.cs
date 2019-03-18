1a81909ec78c25bebd51fa7ef7b577e556ee81fe
LazarPetkovic <42033361+LazarPetkovic@users.noreply.github.com>
2019-03-18 13:34:53 +0100

    Global messages added

---
+++ BookStore.Mvc.Common/WarningMessages.cs
@@ -0,0 +1,10 @@
+﻿namespace BookStore.Mvc.Common
+{
+    public class WarningMessages
+    {
+        public const string QuantityNotEnough = "Quantity is not enough to buy!";
+
+        public const string BookNotFound = "Book does not exist!";
+
+    }
+}
